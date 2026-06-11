52160ae057820b29dc3bb8570beb078360438d55
SHAKTI SINGH SHEKHAWAT <shaktidesktop@gmail.com>
2026-06-11 13:13:49 +0530

    Create 01_variables.js

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId  =144662
+let accountEmail ="shaktidesktop@gmail.com"
+var accountPassword = "12345"
+accountCity ="Jaipur"
+
+// accountId = 2  // not allowed
+accountEmail ="hc@gb.com"
+accountPassword = "56473"
+accountCity = "bengal"
+
+console.log(accountId);
+console.table([accountEmail,accountId,accountPassword,accountCity])
+
+
