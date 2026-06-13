e641d037ec7edfadf2af0ca52a9d1402afbf1a42
SHAKTI SINGH SHEKHAWAT <shaktidesktop@gmail.com>
2026-06-13 21:57:58 +0530

    Create 03_conversionOperation.js

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,33 @@
+let score = true
+//console.log(typeof score) // 33 'number'
+//.log(typeof (score)) // 33 'number'
+
+let valueInNumber = Number(score) // converts the string to a number
+//console.log(typeof valueInNumber) // 33 'number'
+//console.log(valueInNumber) 
+
+
+//"33" -> 33
+//"33ab" -> NaN
+// true -> 1
+// false -> 0
+//undefined -> NaN
+//null -> 0
+
+
+let isLoggedIn = "ssbf"
+
+let bolleanIsLoggedIn = Boolean(isLoggedIn) // converts the number to a boolean
+//console.log(typeof bolleanIsLoggedIn) // 'boolean'
+//console.log(bolleanIsLoggedIn) // true
+
+// 1 = true
+// 0 = false
+// "" = false   
+// "avad" = true
+
+let someNumber = 33
+let stringNumber = String(someNumber) // converts the number to a string
+console.log(typeof stringNumber) // 'string'
+console.log(stringNumber) // "33"
+
