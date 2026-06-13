441be49a6dcc08465177da7acaeec3a5788a2304
SHAKTI SINGH SHEKHAWAT <shaktidesktop@gmail.com>
2026-06-13 22:18:00 +0530

    Update 03_conversionOperation.js

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -28,6 +28,30 @@ let bolleanIsLoggedIn = Boolean(isLoggedIn) // converts the number to a boolean
 
 let someNumber = 33
 let stringNumber = String(someNumber) // converts the number to a string
-console.log(typeof stringNumber) // 'string'
-console.log(stringNumber) // "33"
+//console.log(typeof stringNumber) // 'string'
+//console.log(stringNumber) // "33"
 
+// *******operations********
+
+let value = 3
+let negValue = -value // unary negation
+//console.log(negValue) // -3
+
+
+//console.log((3+3)) // 6
+//console.log((3-3)) // 0
+//console.log((3*3)) // 9
+//console.log((3/3)) // 1
+//console.log(3 % 2) // 1 (modulus operator gives the remainder of the division)
+//console.log(3 ** 2) // 9 (exponentiation operator raises the first operand to the power of the second operand)
+
+
+let str1 = "Hello"
+let str2 = " World"
+let str3 = str1 + str2 // string concatenation
+//console.log(str3) // "Hello World"
+
+console.log("1" + 2) // "12" (string concatenation, number is converted to string)
+console.log(1 + "2") // "12" (string concatenation, number is converted to string)
+console.log("1" + 2 + 3) // "123" (string concatenation, numbers are converted to strings)
+console.log(1 + 2 + "3") // "33" (string concatenation, numbers are added first, then converted to string)
